63f8502f52a52f4a47e20a0da50607740d6a66f6
edson <mbgraficarn@gmail.com>
2023-05-04 21:10:22 -0300

    aula

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,55 @@
 //CODIGO AQUI
+
+let saldo = 15
+saldo+= 22.40
+//console.log(saldo)
+
+saldo -= 12.34
+//console.log(saldo)  
+
+saldo = saldo/2
+//console.log(saldo)
+
+saldo *= 3.40
+//console.log(saldo)
+
+saldo = saldo % 3
+//console.log(saldo)
+
+// PRECEDÊNCIA MATEMÁTICA
+
+let operacao =2*(2+2)
+//console.log(operacao);
+
+const comparacao = 5===5
+//console.log(comparacao);
+
+const comparacao2 =5!==5
+//console.log(comparacao2);
+
+const comparacao3 = 5!=="5"
+//console.log(comparacao3);
+
+const comparacao4 = "5"==="cinco"
+//console.log(comparacao4);
+
+const comparacao5 = typeof 5 === typeof 20
+//console.log(comparacao5);
+
+const comparacao6 = typeof '5' === typeof 'cinco'
+//console.log(comparacao6);
+
+const primeiroNumero = 57
+const segundaNumero = 13
+
+const verificacao = primeiroNumero === segundaNumero
+//console.log(verificacao);
+
+const verificacao2 = primeiroNumero <= segundaNumero
+//console.log(verificacao2);
+
+const verificacao3 = primeiroNumero > segundaNumero
+//console.log(verificacao3);
+
+const verificacao4 = primeiroNumero < segundaNumero
+console.log(verificacao4);
